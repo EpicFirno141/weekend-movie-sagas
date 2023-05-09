d0a02193eb722b865066ad69c19628709873eba8
Kaelan Leahy <kaelan@leahyonline.com>
2023-05-09 01:52:25 -0500

    Added material ui styling to details page.

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,17 +1,26 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
 function Details() {
     const params = useParams();
     const dispatch = useDispatch();
+    const history = useHistory();
     const movie = useSelector(store => store.currentMovie)
+
+    const backButton = () => {
+        console.log('test back');
+        history.push('/');
+    }
 
     useEffect(() => {
         console.log(params);
@@ -19,25 +28,49 @@ function Details() {
     }, [])
 
     return(
-        <Box>
-            <Card sx={{ maxWidth: 750, m: 5 }} elevation={3}>
-                <CardMedia
-                    sx={{ height: 'auto' }}
-                    image={movie.poster}
-                    title={movie.title}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {movie.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {movie.description}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {movie.genres}
-                    </Typography>
-                </CardContent>
-            </Card>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    <Card sx={{ mt: 5, ml: 5 }} elevation={3}>
+                        <CardMedia
+                            component="img"
+                            height="auto"
+                            sx={{ objectFit: "contain" }}
+                            image={movie.poster}
+                            title={movie.title}
+                        />
+                    </Card>
+                </Grid>
+                <Grid item xs={6}>
+                    <Card sx={{ m: 5 }} elevation={3}>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {movie.title}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {movie.description}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper sx={{ maxWidth: 'auto', mt: 5, mr:5, p: 3}} elevation={3}>
+                        <Button variant="contained" sx={{ px: 4, py: 1 }} onClick={() => {
+                            history.push('/');
+                        }}>Back</Button>
+                    </Paper>
+                    <Card sx={{ maxWidth: 'auto', mt: 5, mr:5, p: 3}} elevation={3}>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Genres:
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {movie.genres}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
         </Box>
     );
 }
