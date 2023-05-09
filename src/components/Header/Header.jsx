767c6681eee3e7978acee02c8f8d0667cdc12b27
Kaelan Leahy <kaelan@leahyonline.com>
2023-05-09 02:25:58 -0500

    Added movie icon to header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,25 @@
 import Paper from '@mui/material/Paper'
 import { useState } from 'react';
-
+import MovieIcon from '@mui/icons-material/Movie';
+import { Grid } from '@mui/material';
 
 function Header() {
     return(
-        <Paper square elevation={3} sx={{ p: 3, fontSize: 32, textAlign: 'left'}}>Movies</Paper>
+        <Paper square elevation={3} sx={{ p: 3, fontSize: 32, textAlign: 'left'}}>
+            <Grid
+                container
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="center"
+            >
+                <Grid item xs={1}>
+                    <MovieIcon sx={{ fontSize: 48}} />
+                </Grid>
+                <Grid item xs={1}>
+                    Movies
+                </Grid>
+            </Grid>
+        </Paper>
     );
 }
 
